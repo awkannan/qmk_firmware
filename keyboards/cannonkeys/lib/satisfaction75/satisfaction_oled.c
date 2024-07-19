a52c693f6b0d1f773b8c09779d8699967c6fde6c
Andrew Kannan <andrew.kannan@gmail.com>
2024-07-19 16:52:14 -0400

    Add friendly cat code to sat75

--- keyboards/cannonkeys/lib/satisfaction75/satisfaction_oled.c
+++ keyboards/cannonkeys/lib/satisfaction75/satisfaction_oled.c
@@ -4,6 +4,11 @@
 #include "satisfaction_core.h"
 #include <stdio.h>
 
+#ifdef BONGO_ENABLE
+#include "bongo.h"
+#endif
+
+
 void draw_default(void);
 void draw_clock(void);
 
@@ -29,6 +34,14 @@ bool oled_task_kb(void) {
         case OLED_TIME:
             draw_clock();
             break;
+        #ifdef BONGO_ENABLE
+        case OLED_BONGO:
+            draw_bongo(false);
+            break;
+        case OLED_BONGO_MIN:
+            draw_bongo(true);
+            break;
+        #endif
     }
     return false;
 }
